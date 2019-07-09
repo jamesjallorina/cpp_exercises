472ae5171b4934bd105a2f7011c5d4dfcc2af36e
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-07-08 18:09:51 -0700

    added source code solution for exercise 7

---
+++ exercise_part_1/exercise7/user_input_line_display.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main( int argc, char **argv )
+{
+	std::string buffer = "";	
+	std::string filename = ""; 	
+	std::ifstream filestream;
+	
+	if( argc < 2 )
+	{
+		std::cout << "./user_input_line_display [ filename ] " << "\n";
+		return (-1);
+	}
+	
+	filename = argv[1];
+	filestream.open( filename.c_str(), std::ifstream::in);
+
+	if( filestream.good() )
+	{
+		std::cout << "file is good " << "\n";
+		if( !filestream.is_open() )
+		{
+			std::cout << "could not open file " << "\n";
+			return (-1);
+		}
+		std::cout << "file open is okay " << "\n";
+	}
+	
+	while( std::getline( filestream, buffer ) )
+	{
+		std::cout << buffer << "\n";
+		std::cout << "press enter to continue . . . \n";
+		std::getchar( );
+	}
+	return 0;
+}
