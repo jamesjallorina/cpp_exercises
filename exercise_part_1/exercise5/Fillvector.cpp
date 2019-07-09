4447ed6350d9ced4e6c8c2d3c22590d3e21e4ada
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-07-08 17:58:56 -0700

    added source code solution for exercise 5

--- exercise_part_1/exercise5/Fillvector.cpp
+++ exercise_part_1/exercise5/Fillvector.cpp
@@ -5,16 +5,18 @@
 #include <fstream>
 #include <vector>
 
-using namespace std;
-
-int main() 
+int main( ) 
 {
-	vector<string> v;
-	ifstream in("Fillvector.cpp");
-	string line;
-	while(getline(in, line))
-	v.push_back(line); // Add the line to the end
+	std::vector< std::string > v;
+	std::ifstream in("Fillvector.cpp");
+	std::string line;
+	while( std::getline( in, line ) )
+		v.push_back(line); // Add the line to the end
+	
 	// Add line numbers:
-	for(int i = v.size(); i != 0; --i)
-	cout << i << ": " << v[i] << endl;
+	//for(int i = v.size(); i != 0; --i)
+	//cout << i << ": " << v[i] << endl;
+	int i = v.size();
+	for( std::vector< std::string >::const_reverse_iterator itr = v.rbegin(); itr != v.rend(); ++itr, --i )
+		std::cout << i << ": " << *itr << "\n"; 
 } ///:~
