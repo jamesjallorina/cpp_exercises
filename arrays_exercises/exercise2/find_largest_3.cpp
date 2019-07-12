7323984d8d93d5159de57a3b300b7ca3a1c4de37
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-07-11 19:36:23 -0700

    added source code solution for exercise 2

---
+++ arrays_exercises/exercise2/find_largest_3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdarg>
+
+const int move_index = 2;
+
+int * parse_arguments( char **number )
+{
+	int *_arr = NULL;
+	int arguments = atoi( number[1] );	
+	int value = 0;
+
+	_arr = new int [ arguments ];
+	for( int i = 0; i < arguments; ++i )
+	{
+		value = atoi( number[ i + move_index ] );	
+		_arr[ i ] = value;
+	}	
+	return _arr;
+}
+
+int main( int argc, char **argv )
+{
+	int arguments = 0;
+	int *_arr = NULL;
+	
+	int largest1 = 0;
+	int largest2 = 0;
+	int largest3 = 0;
+		
+	if( argc < 2 )
+	{
+		std::cout << "./find_largest [ total number arguments +  integer arguments . . . ] \n"; 
+		return ( -1 );
+	}
+	arguments = atoi( argv[ 1 ] );
+	_arr = parse_arguments( argv );
+	largest1 = 0; 	// the most largest
+	largest2 = 0;	// second largest
+	largest3 = 0; 	// third largest
+	/* main algorithm for exercise 1 */	
+	for( int i = 0; i < arguments; ++i )
+	{
+		if( largest1 < _arr[ i ] )
+			largest1 = _arr[ i ];
+		else if (largest2 < _arr[ i ] )
+		{
+			largest3 = largest2; 
+			largest2 = _arr[ i ];
+		}
+	}
+	std::cout << "largest value  : " << largest1 << "\n";
+	std::cout << "second largest : " << largest2 << "\n";
+	std::cout << "third largest  : " << largest3 << "\n";
+	/* end of algorithm */
+	return 0;
+
+}
